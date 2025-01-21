e42eace549dffb59c2077c8622ee2ba871e22454
vuongprogk <vuong753333@gmail.com>
2025-01-21 17:09:53 +0700

    feat(UserService): add prototype for user role repository

---
+++ backend/UserService/Infrastructure/Repository/UserRoleRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repository
+{
+    public class UserRoleRepository : IUserRoleRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UserRoleRepository(ApplicationDbContext db) { _db = db; }
+        public Task<bool> AssignRole(string userId, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GrantedDefaultRole(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RevokeRole(string userId, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
