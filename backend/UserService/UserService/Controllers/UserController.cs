0aa4a87856535d085ecceb2a18e05ce6a5efe164
vuongprogk <vuong753333@gmail.com>
2025-01-21 17:02:42 +0700

    feat(UserService): add user controller prototype

---
+++ backend/UserService/UserService/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Query;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _db;
+        public UserController(IUserRepository db)
+        {
+            _db = db;
+        }
+        [HttpGet("users")]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _db.GetAllAsync(UserQuery.GetAllUser);
+            return Ok(users);
+        }
+    }
+}
