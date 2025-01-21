26a01d42b8c912b8a60d95f7229ca14164367461
vuongprogk <vuong753333@gmail.com>
2025-01-21 17:04:03 +0700

    fix(UserService): update request objs

--- backend/UserService/Application/Features/Auth/Commands/LoginRequest.cs
+++ backend/UserService/Application/Features/Auth/Commands/LoginRequest.cs
@@ -1,7 +1,11 @@
-﻿namespace Application.Features.Auth.Commands;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Features.Auth.Commands;
 
 public class LoginRequest
 {
+    [Required]
     public string Email { get; set; }
+    [Required]
     public string Password { get; set; }
 }
--- backend/UserService/Application/Features/Auth/Commands/RegisterRequest.cs
+++ backend/UserService/Application/Features/Auth/Commands/RegisterRequest.cs
@@ -1,7 +1,18 @@
-﻿namespace Application.Features.Auth.Commands;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Features.Auth.Commands;
 
 public class RegisterRequest
 {
-    public string Email { get; set; }
-    public string Password { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    [MinLength(8, ErrorMessage = "Your password must be longer than 8 characters.")]
+    public string Password { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+
+
 }
