918e6bf5cf8250d8d1724f9f8debe2c2c95c541e
vuongprogk <vuong753333@gmail.com>
2025-01-21 17:11:36 +0700

    refactor(UserService): implement auth controller

--- backend/UserService/UserService/Controllers/AuthController.cs
+++ backend/UserService/UserService/Controllers/AuthController.cs
@@ -1,15 +1,28 @@
 ﻿using Application.Features.Auth.Commands;
+using Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UserService.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("auth")]
 public class AuthController : ControllerBase
 {
+    private readonly IAuthService _auth;
+    public AuthController(IAuthService authService)
+    {
+        _auth = authService;
+    }
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
     {
-        return Ok();
+        var result = await _auth.LoginAsync(loginRequest);
+        return Ok(result);
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest){
+        var result = await _auth.RegisterAsync(registerRequest);
+        return Ok(result);
     }
 }
