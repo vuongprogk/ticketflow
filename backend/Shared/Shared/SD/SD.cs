d7c838a95ae08a17f74b299c30c0e8e7d402947e
vuongprogk <vuong753333@gmail.com>
2025-01-21 17:21:44 +0700

    feat(Shared): add static data for role

---
+++ backend/Shared/Shared/SD/SD.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shared.SD
+{
+    public static class SD
+    {
+        public const string RoleName = "role";
+    }
+}
