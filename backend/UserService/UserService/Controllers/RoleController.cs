033dbe3be1719d8069851fe7cbb30b429c2020d1
vuongprogk <vuong753333@gmail.com>
2025-01-21 17:00:27 +0700

    feat(UserService): add role and userrole controller prototype

---
+++ backend/UserService/UserService/Controllers/RoleController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("api/role")]
+    public class RoleController: ControllerBase
+    {
+    }
+}
---
+++ backend/UserService/UserService/Controllers/UserRoleController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("api/userrole")]
+    public class UserRoleController: ControllerBase
+    {
+        
+    }
+}
