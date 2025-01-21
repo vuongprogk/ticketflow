da15a71594b94e6bc3add961de9e8b1d719b3c04
vuongprogk <vuong753333@gmail.com>
2025-01-21 17:09:23 +0700

    fix(UserService): update attribute of user model

--- backend/UserService/Domain/Models/User.cs
+++ backend/UserService/Domain/Models/User.cs
@@ -2,7 +2,7 @@
 
 public class User
 {
-  public string Id { get; set; } = Guid.NewGuid().ToString();
+  public Guid Id { get; set; } = Guid.NewGuid(); 
   public string Name { get; set; } = string.Empty;
   public string Email { get; set; } = string.Empty;
   public string PasswordHash { get; set; } = string.Empty;
