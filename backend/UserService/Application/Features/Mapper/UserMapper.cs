72128575a3558ec2e2e7265650be6ac96b9d7f9a
vuongprogk <vuong753333@gmail.com>
2025-01-21 17:06:50 +0700

    feat(UserService): add mapper to mapping register request to user

---
+++ backend/UserService/Application/Features/Mapper/UserMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Features.Auth.Commands;
+using AutoMapper;
+using BCrypt.Net;
+using Domain.Models;
+
+namespace Application.Features.Mapper
+{
+    public class UserMapper : Profile
+    {
+        public UserMapper()
+        {
+            CreateMap<RegisterRequest, User>().ForMember(u => u.PasswordHash,
+                option => option.MapFrom(request => request.Password
+            ));
+        }
+    }
+}
